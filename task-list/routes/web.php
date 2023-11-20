a7f5faf44c67195c5480a91e42818b707be5a750
Nguyen Duong <duongnguyen02102001@gmail.com>
2023-11-20 16:26:45 +0700

    add some route with name

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -15,4 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('task.index');
+
+Route::get('/hello', function () {
+    return 'Hello Page';
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello' . $name . '!';
+});
+
+Route::get('/hallo', function () {
+    return redirect()->route('task.index');
+});
+
+Route::fallback(function () {
+    return 'Still got somewhere';
 });
